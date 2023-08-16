0c97eabbab9ff699936eb02977867c52aaafad1e
linda <lindacristy667@gmail.com>
2023-08-16 15:40:19 -0400

    primiro commit

---
+++ somanum.py
@@ -0,0 +1,8 @@
+#Script python que leia dois numeros e tenta mostrar a soma entre elas.
+
+num1= int(input('Digite o primeiro numero:'))
+num2= int(input('Digite o segundo numero:'))
+
+soma = num1 + num2
+
+print(f'A soma de {num1} + {num2} é:{soma}')
