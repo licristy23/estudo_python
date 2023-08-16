a0003af53e7e5e98fca60348f77dc0d0f9db36f8
linda <lindacristy667@gmail.com>
2023-08-16 15:48:13 -0400

    Dados de pessoas

---
+++ dados.py
@@ -0,0 +1,11 @@
+nome = input ('QUal seu nome?')
+idade = input('Idade?')
+peso = input('E seu peso?')
+
+print(nome, idade, peso)
+
+#como usar o print
+#usar variavel
+#atribuir valores a essas variaves
+#como mostrar essas varias com print usando virgula e com +
+#utilizar o input pra interagir com o usuario 
