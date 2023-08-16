c3df525117e1aa087624349e8585c4b8eb33837f
linda <lindacristy667@gmail.com>
2023-08-16 15:44:06 -0400

    Dia nascimento

---
+++ dianasc.py
@@ -0,0 +1,7 @@
+#Script python que leia o dia, mes e ano de nascimento e mostra a mensagem da data formada
+
+dia = input('Dia do seu nascimento:')
+mes = input('Mes:')
+ano = input('Ano:')
+
+print('Você nasceu dia', dia, 'do mês', mes, 'do ano', ano)
